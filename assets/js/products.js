d19aa0b8913ff747a2ed67b32823c418214322bf
cosminuity <cosminuity@gmail.com>
2026-01-05 03:39:16 -0500

    Create products.js

---
+++ assets/js/products.js
@@ -0,0 +1,21 @@
+const products = [
+  {
+    id: "w24-platypys-banana",
+    name: "Banana Platypys",
+    season: "Winter 2024",
+    status: "sold",
+    image: "assets/images/store/w24-platypys-banana.jpg",
+    heroPos: "50% 50%",
+    href: "products/w24-platypys-banana.html"
+  },
+  {
+    id: "w24-platypys-cos",
+    name: "Cos Platypys",
+    season: "Winter 2024",
+    status: "sold",
+    image: "assets/images/store/w24-platypys-platypys-cos.jpg",
+    heroPos: "50% 50%",
+    href: "products/w24-platypys-cos.html"
+  }
+  // more products
+]
